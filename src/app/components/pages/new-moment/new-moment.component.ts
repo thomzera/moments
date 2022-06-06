1676eab70f513c647560ba807c219baf3caa46cb
Thomas Koch <thomaskoch@usp.br>
2022-06-06 15:44:14 -0300

    finalizando sistema de mensagens com exibição de mensagem 'popup' e redirect para a home

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment'
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -12,7 +14,11 @@ export class NewMomentComponent implements OnInit {
 
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -34,7 +40,9 @@ export class NewMomentComponent implements OnInit {
     await this.momentService.createMoment(formData).subscribe();
 
     // exibir msg
+    this.messagesService.add('Momento adicionado com sucesso!')
 
     // redirect
+    this.router.navigate(['/']);
   }
 }
