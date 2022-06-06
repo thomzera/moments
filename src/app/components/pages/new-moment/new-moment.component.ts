b56bc7a414d20e7ad9c6670d8efd3b3e4903e3ff
Thomas Koch <thomaskoch@usp.br>
2022-06-06 14:57:42 -0300

    configurando o sistema para inserção de dados

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Moment } from 'src/app/Moment'
+import { MomentService } from 'src/app/services/moment.service';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -9,9 +12,29 @@ export class NewMomentComponent implements OnInit {
 
   btnText = 'Compartilhar!';
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
   }
 
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image) {
+      formData.append("image", moment.image)
+    }
+
+    // todo
+
+    // enviar para o service
+    await this.momentService.createMoment(formData).subscribe();
+
+    // exibir msg
+
+    // redirect
+  }
 }
